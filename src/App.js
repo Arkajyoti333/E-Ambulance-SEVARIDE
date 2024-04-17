f1f5ae18567142822d9993adcdfdb55d1ede8456
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-04-17 17:37:26 +0530

    set homepage in  app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import Home from "./components/Home/homescreen";
-import Footer from "./components/Footer/footer";
-import Navbar from "./components/Navbar/navbar";
 import Services from "./components/Services/services"
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+  
       <Home/>
       <Services/> 
-      <Footer/>
+    
       
     </div>
   );
